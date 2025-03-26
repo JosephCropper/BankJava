798ece27fa871d76849e31cfd908881611db6579
Jo Cropper <joseph.cropper@bsu.edu>
2025-03-25 23:23:37 -0400

    Create Admin.java

---
+++ code/Admin.java
@@ -0,0 +1,7 @@
+public class Admin extends User{
+    public Admin(){}
+    public String menu(){return "noreturngiven";}
+    public String getReport(){return "noreturngiven";}
+    public void start(){}
+
+}
