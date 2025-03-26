7dd07e3cc5254a8fcdb7d8aa8752b48ab2a3a3ed
Jo Cropper <joseph.cropper@bsu.edu>
2025-03-25 23:23:23 -0400

    Create Bank.java

---
+++ code/Bank.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Bank {
+    private Admin admin;
+    private ArrayList<Customer> customers;
+
+    public Bank(){}
+    public void main(){}
+    public void loadSampleCustomers(){}
+    public void loadCustomers(){}
+    public void saveCustomers(){}
+    public void fullCustomerReport(){}
+    public void addUser(){}
+    public void applyInterest(){}
+    public void loginAsCustomer(){}
+    public void menu(){}
+    public void start(){}
+    public void startAdmin(){}
+    
+}
