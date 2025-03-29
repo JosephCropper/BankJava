e5353ed02bf5b5be3575746ccf3b91b26403c2df
Jo Cropper <joseph.cropper@bsu.edu>
2025-03-28 22:36:31 -0400

    Create Main.java

---
+++ codeForPart2/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+//heads up:
+//i use "this." a lot in java. its a nervous habit that hasn't broken anything yet. if i feel like im gonna confuse something,
+//and i throw it in, and it still works, im just keeping it. if given proper reason to stop, i will.
+public class Main {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        bank.start();
+
+    }
+}
